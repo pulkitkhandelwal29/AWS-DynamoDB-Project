147d58ee674adbb3ff4eafe0b7b32ef66b1222a2
Pulkit Khandelwal <67990422+pulkitkhandelwal29@users.noreply.github.com>
2021-09-02 22:31:57 +0530

    Creating Table

---
+++ create_data.py
@@ -0,0 +1,36 @@
+import boto3  # import Boto3
+
+
+def create_devices_table(dynamodb=None):
+    dynamodb = boto3.resource(
+        'dynamodb', endpoint_url="http://localhost:8000")
+    # Table defination
+    table = dynamodb.create_table(
+        TableName='SmartFarming',
+        KeySchema=[
+            {
+                'AttributeName': 'device_id',
+                'KeyType': 'HASH'  # Partition key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'device_id',
+                # AttributeType defines the data type. 'S' is string type and 'N' is number type
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            # ReadCapacityUnits set to 10 strongly consistent reads per second
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10  # WriteCapacityUnits set to 10 writes per second
+        }
+    )
+    return table
+
+
+if __name__ == '__main__':
+    device_table = create_devices_table()
+    # Print tablle status
+    print('Table Created')
+    print("Status:", device_table.table_status)
